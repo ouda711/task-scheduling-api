97c56cf673bd5eeac90b31a4d498b06d71c95e80
ouda211 <ouda.wycliffe@gmail.com>
2022-03-21 20:42:55 +0300

    Fixed config dialect option

--- config/config.js
+++ config/config.js
@@ -19,6 +19,9 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": true
+    }
   }
 }
