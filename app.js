c8af0454fcf60422907e202d8104a72aac0c5115
ouda211 <ouda.wycliffe@gmail.com>
2022-03-24 00:01:02 +0300

    Fixed cors issue

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/user.routes');
@@ -17,6 +18,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors())
 app.use(BenchMarkMiddleware.benchmark);
 app.use(logger('dev'));
 app.use(express.json());
