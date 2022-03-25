ffb1b041ebd97e55e3887cb1ec0170bcdae47306
ouda211 <ouda.wycliffe@gmail.com>
2022-03-25 20:25:47 +0300

    Fixed cors errors

--- app.js
+++ app.js
@@ -20,16 +20,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(cors())
+app.options('*', cors());
 app.use(BenchMarkMiddleware.benchmark);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/api/v1/users', usersRouter);
-app.use('/api/v1/tasks', tasksRouter);
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
@@ -39,6 +36,10 @@ app.use(function(req, res, next) {
   next();
 });
 
+
+app.use('/', indexRouter);
+app.use('/api/v1/users', usersRouter);
+app.use('/api/v1/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
