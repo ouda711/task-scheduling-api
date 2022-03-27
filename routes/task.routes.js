de0013e7d4d75a2e02515e94e8d672a786f2dfcf
ouda211 <ouda.wycliffe@gmail.com>
2022-03-27 14:04:23 +0300

    Fixed route protector

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -3,7 +3,7 @@ const controller =  require('../controllers/task.controller');
 require('./param_loaders/task.loader').init(router);
 const AuthMiddleware = require('../middlewares/auth.middleware');
 
-router.get('', AuthMiddleware,controller.getAll);
-router.get('/by_id/:taskId', AuthMiddleware, controller.getById);
+router.get('',controller.getAll);
+router.get('/by_id/:taskId', controller.getById);
 
 module.exports = router
